6601568c4089baaf2d1b140fba87c337abf15595
rajatwanere <rajatwanere999@gmail.com>
2023-06-06 16:22:02 +0530

    added springseason.java

---
+++ Springseason.java
@@ -0,0 +1,15 @@
+package day8assignment;
+
+public class Springseason {
+    public static void main(String[] args) {
+        int month =Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
+
+        boolean flag = (month ==3 && day >=20 && day <=31)
+                          || (month ==4 && day >=1 && day <=30)
+                          ||((month ==5 && day >=1 && day <=31))
+                          ||((month ==6 && day >=1 && day <=20));
+
+        System.out.println(flag);
+    }
+}
